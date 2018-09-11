6e8c54e8b9fad975b7cbe36ac3b08801ff933fe4
llJaenll <33041976+llJaenll@users.noreply.github.com>
2018-09-11 17:24:30 -0500

    PrimeraSubida
    PrimeraSubida11-09-2018

---
+++ webapp/controller/Inicio.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("NombProy.Nomb_Proy.controller.Inicio", {
+		onInit: function(){
+			
+		}
+	});
+});
